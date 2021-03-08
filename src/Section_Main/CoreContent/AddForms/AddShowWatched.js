61c6ea179524d3c8097d9836ad1c15618fb517a2
Sam Cutler <sam.cutler@gmail.com>
2021-03-08 10:48:44 -0800

    Reformatted date passed to new Date() method

--- src/Section_Main/CoreContent/AddForms/AddShowWatched.js
+++ src/Section_Main/CoreContent/AddForms/AddShowWatched.js
@@ -14,7 +14,7 @@ function AddShowWatched() {
         const { title, service, genre, month, year, rating } = e.target;
 
         // Combine month/year inputs into a UTC-formatted date
-        const startDate = `${year.value}-${month.value}-15`;
+        const startDate = `${year.value}-${month.value}-15T00:00:00`;
          console.log(startDate);
         const intDate = new Date(startDate);
          console.log(intDate)
@@ -85,30 +85,30 @@ function AddShowWatched() {
                         <div className='date-component'>
                             <label htmlFor='month'>Month:</label><br />
                             <select name='month' id='month'>
-                                <option value='Jan'>Jan</option>
-                                <option value='Feb'>Feb</option>
-                                <option value='Mar'>Mar</option>
-                                <option value='Apr'>Apr</option>
-                                <option value='May'>May</option>
-                                <option value='Jun'>Jun</option>
-                                <option value='Jul'>Jul</option>
-                                <option value='Aug'>Aug</option>
-                                <option value='Sep'>Sep</option>
-                                <option value='Oct'>Oct</option>
-                                <option value='Nov'>Nov</option>
-                                <option value='Dec'>Dec</option>
+                                <option value='01'>Jan</option>
+                                <option value='02'>Feb</option>
+                                <option value='03'>Mar</option>
+                                <option value='04'>Apr</option>
+                                <option value='05'>May</option>
+                                <option value='06'>Jun</option>
+                                <option value='07'>Jul</option>
+                                <option value='08'>Aug</option>
+                                <option value='09'>Sep</option>
+                                <option value='10'>Oct</option>
+                                <option value='11'>Nov</option>
+                                <option value='12'>Dec</option>
                             </select>
                         </div>
                         <div className='date-component'>
                             <label htmlFor='year'>Year:</label><br />
                             <select name='year' id='year'>
-                                <option value='2015'>2015</option>
-                                <option value='2016'>2016</option>
-                                <option value='2017'>2017</option>
-                                <option value='2018'>2018</option>
-                                <option value='2019'>2019</option>
-                                <option value='2020'>2020</option>
                                 <option value='2021'>2021</option>
+                                <option value='2020'>2020</option>
+                                <option value='2019'>2019</option>
+                                <option value='2018'>2018</option>
+                                <option value='2017'>2017</option>
+                                <option value='2016'>2016</option>
+                                <option value='2015'>2015</option>
                             </select>
                         </div>
                     </div>
